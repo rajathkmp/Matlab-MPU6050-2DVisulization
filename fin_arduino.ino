7744767e5a1ba587cdb18c988690aa7986d112e3
Rajath Kumar M.P <rajathkumarmp1995@gmail.com>
2015-11-11 22:40:30 +0530

    edit

--- fin_arduino.ino
+++ fin_arduino.ino
@@ -4,28 +4,21 @@
 #include "I2Cdev.h"
 #include "MPU6050.h"
 #include "Servo.h"
-
 MPU6050 mpu;
-
-int16_t ax, ay, az;
-int16_t gx, gy, gz;
-
-
-long val, val2;
+int16_t a, b, c;
+int16_t d, e, f;
+long res, val2;
 long prevVal;
-
 void setup() 
 {
     Wire.begin();
     Serial.begin(9600);
     mpu.initialize();
-    
 }
-
 void loop() 
 {
-    mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-    val = map(ay, -17000, 17000, 0, 180);
-    Serial.println(val);
-    delay(50);
+    mpu.getMotion6(&a, &b, &c, &d, &e, &f);
+    res = map(b, -17000, 17000, 0, 180);
+    Serial.println(res);
+    delay(10);
 }
