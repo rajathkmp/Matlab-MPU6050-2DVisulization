efa8ea09bd4d84482ca6b8b429768f0a68e843d5
Rajath Kumar M.P <rajathkumarmp1995@gmail.com>
2015-11-11 22:42:18 +0530

    edit

--- matlab.m
+++ matlab.m
@@ -1,45 +1,17 @@
-
-
-
-
-
-clear all
-close all
-clc
-
-
-
-
-
-
 arduin=serial('COM3','BaudRate',9600);
 fopen(arduin);
-
-
-
 CM(1)=0;
 time(1)=0;
 x=[-0.5 0.5 0.5 -0.5; -1 -1 1 1];
-
 tic;
-
-
 while (toc<=20)  
-    
-
-   theta=fscanf(arduin,'%f');
- 
-
-th= -pi*theta/180;
+    theta=fscanf(arduin,'%f');
+ th= -pi*theta/180;
 xn=[cos(th) -sin(th); sin(th) cos(th)]*x;
 figure(1)
 xx=fill(xn(1,:),xn(2,:), 'r');
 axis([-2, 2, -2, 2])
-
-
- 
 end
- 
 fclose(arduin); 
 
 
